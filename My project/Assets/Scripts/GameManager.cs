8b4703dc0829e7607286d8bcb76b5cfaa0bc2430
MarcoNoir <s.aurelio@escolar.ifrn.edu.br>
2023-11-10 12:58:52 -0300

    Final motores

--- My project/Assets/Scripts/GameManager.cs
+++ My project/Assets/Scripts/GameManager.cs
@@ -5,15 +5,26 @@ using TMPro;
 
 public class GameManager : MonoBehaviour
 {
-
-public TextMeshProUGUI hud, msgVitoria;
-public int restantes;
+    public TextMeshProUGUI hud, msgvitoria;
+    public int restantes;
 
 
     // Start is called before the first frame update
     void Start()
     {
         restantes = FindObjectsOfType<Moeda>().Length;
+
+        hud.text = $"Akumas restantes: {restantes}";
+    }
+
+    public void SubtrairMoedas(int valor){
+        restantes -= valor;
+         hud.text = $"Akumas restantes: {restantes}";
+         
+        if(restantes <= 0 ){
+            //ganhou o jogo
+            msgvitoria.text = "Parabéns! Isso é miraculoso! Zerou!!";
+        }
     }
 
     // Update is called once per frame
